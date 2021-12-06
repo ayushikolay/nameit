b371bedd5c7eddc6721b3cf0351cf5f9450e5f98
Ayushi Kolay <ayushi18071998@gmail.com>
2021-12-06 12:52:34 +0530

    head component completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello 
+      <Header />
     </div>
   );
 }
